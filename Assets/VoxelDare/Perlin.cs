28ab4a6a9b9bdbada274b64e12ec023857dc47e4
David W <davidw@danvil.de>
2014-09-02 13:42:41 +0200

    Improved comments and formatting.

--- Assets/VoxelDare/Perlin.cs
+++ Assets/VoxelDare/Perlin.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 
 /// <summary>
-/// Perlin noise after Ken Perlin's reference implementation.
+/// Improved perlin noise after Ken Perlin's reference implementation.
+/// See http://mrl.nyu.edu/~perlin/noise/
 /// Implemented 2014 by David Weikersdorfer
 /// </summary>
 public class Perlin
@@ -62,12 +63,17 @@ public class Perlin
 	
 	public float Compute(UnityEngine.Vector3 v)
 	{
-		return Compute (v.x, v.y, v.z);
+		return Compute(v.x, v.y, v.z);
+	}
+	
+	public float Compute(float x, float y, float z)
+	{
+		return Compute(x, y, z, this.persistence);
 	}
 	
 	public float Compute(UnityEngine.Vector3 v, float[] persistence)
 	{
-		return Compute (v.x, v.y, v.z, persistence);
+		return Compute(v.x, v.y, v.z, persistence);
 	}
 	
 	public float Compute(float x, float y, float z, float[] persistence)
@@ -83,74 +89,64 @@ public class Perlin
 		return noise;
 	}
 	
-	public float Compute(float x, float y, float z)
-	{
-		return Compute(x, y, z, this.persistence);
-	}
-	
 	private float Noise(float x, float y, float z)
 	{
-		// round coordinates to next integer
-		int ifx = (int)Math.Floor(x);
-		int ify = (int)Math.Floor(y);
-		int ifz = (int)Math.Floor(z);
+		// find unit cube that contains point
+		int ifx = UnityEngine.Mathf.FloorToInt(x);
+		int ify = UnityEngine.Mathf.FloorToInt(y);
+		int ifz = UnityEngine.Mathf.FloorToInt(z);
 		
-		// integer cube coordinates
+		// wrap to fit into entropy vector
 		int iX = ifx & 255;
 		int iY = ify & 255;
 		int iZ = ifz & 255;
 		
-		// relative cube coordinates
+		// find relative x,y,z of point in cube
 		x -= (float)ifx;
 		y -= (float)ify;
 		z -= (float)ifz;
 		
-		// fade curves
+		// compute fade curves for each of x,y,z
 		float u = Fade(x);
 		float v = Fade(y);
 		float w = Fade(z);
 		
-		// Hash coordinates of the 8 cube corners
-		int A = p[iX] + iY;
-		int AA = p[A] + iZ;
-		int AB = p[A + 1] + iZ;
-		int B = p[iX + 1] + iY;
-		int BA = p[B] + iZ;
-		int BB = p[B + 1] + iZ;
+		// hash coordinates of the 8 cube corners
+		int A = p[iX  ] + iY;
+		int AA = p[A  ] + iZ;
+		int AB = p[A+1] + iZ;
+		int B = p[iX+1] + iY;
+		int BA = p[B  ] + iZ;
+		int BB = p[B+1] + iZ;
 		
-		// And add blended results from 8 corners of cube.
+		// add blended results from 8 corners of cube
 		return
-			Lerp(w,
-			     Lerp(v,
-			     Lerp(u,
-			     Grad(p[AA], x, y, z),
-			     Grad(p[BA], x - 1, y, z)),
-			     Lerp(u,
-			     Grad(p[AB], x, y - 1, z),
-			     Grad(p[BB], x - 1, y - 1, z))),
-			     Lerp(v,
-			     Lerp(u,
-			     Grad(p[AA + 1], x, y, z - 1),
-			     Grad(p[BA + 1], x - 1, y, z - 1)),
-			     Lerp(u,
-			     Grad(p[AB + 1], x, y - 1, z - 1),
-			     Grad(p[BB + 1], x - 1, y - 1, z - 1))));
+			Lerp(w, Lerp(v, Lerp(u, Grad(p[AA  ], x  , y  , z  ),
+									Grad(p[BA  ], x-1, y  , z  )),
+							Lerp(u, Grad(p[AB  ], x  , y-1, z  ),
+									Grad(p[BB  ], x-1, y-1, z  ))),
+					Lerp(v, Lerp(u, Grad(p[AA+1], x  , y  , z-1),
+									Grad(p[BA+1], x-1, y  , z-1)),
+							Lerp(u, Grad(p[AB+1], x  , y-1, z-1),
+									Grad(p[BB+1], x-1, y-1, z-1))));
 	}
-	
+
+	// smooth and differentiable fade function 0 -> 1
 	private static float Fade(float t)
 	{
-		return t*t*t*(t*(t*6 - 15) + 10);
+		return t*t*t*(t*(t*6.0f - 15.0f) + 10.0f);
 	}
-	
+
+	// linear interpolation
 	private static float Lerp(float p, float a, float b)
 	{
 		return a + p*(b - a);
 	}
-	
-	private static float Grad(int hashCode, float x, float y, float z)
+
+	// convert lower 4 bits of hash code into 12 gradient directions
+	private static float Grad(int hash, float x, float y, float z)
 	{
-		// convert lower 4 bits of hash code into 12 gradient directions
-		int h = hashCode & 15;
+		int h = hash & 15;
 		float u = h < 8 ? x : y;
 		float v = h < 4 ? y : (h == 12 || h == 14 ? x : z);
 		return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
