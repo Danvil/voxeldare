11493820c55953bc968d8b72b1672a62146ee5a6
David W <davidw@danvil.de>
2014-09-02 17:04:05 +0200

    Simplified world generator

--- Assets/VoxelDare/Examples/WorldGenerator.cs
+++ Assets/VoxelDare/Examples/WorldGenerator.cs
@@ -5,12 +5,12 @@ using System;
 
 public class WorldGenerator : MonoBehaviour
 {
-	public int worldSize = 32;
-	public int worldHeight = 6;
+	public int worldSize = 64;
+	public int worldHeight = 16;
 
-	public enum WorldType { MINI_MINECRAFT, DISCWORLD, ROLLINGHILLS };
-	public WorldType worldType;
-
+	const int WATER_HEIGHT = 4;
+	const float XY_SCALE = 2.0f;
+	
 	Perlin perlin;
 
 	VoxelDare.Voxel vAir, vWater, vLand, vBedRock;
@@ -24,72 +24,7 @@ public class WorldGenerator : MonoBehaviour
 		vBedRock = new VoxelDare.Voxel(VoxelDare.Voxel.Solidness.Ultra, new Color(0.21f,0.22f,0.22f));
 	}
 
-	public VoxelDare.World Create(VoxelDare.Int3 min, VoxelDare.Int3 max, Vector3 scale, Func<int,int,int,VoxelDare.Voxel> f)
-	{
-		VoxelDare.World w = new VoxelDare.World(scale);
-		for(int z=min.z; z<max.z; z++) {
-			for(int y=min.y; y<max.y; y++) {
-				for(int x=min.x; x<max.x; x++) {
-					w.Set(new VoxelDare.Int3(x,y,z), f(x,y,z));
-				}
-			}
-		}
-		return w;
-	}
-
-	const int WATER_HEIGHT = 4;
-	const float XY_SCALE = 2.0f;
-
-	VoxelDare.Voxel FMiniMinecraft(int x, int y, int z)
-	{
-		float q = Mathf.Max(0, 5*(1+perlin.Compute(XY_SCALE*x,XY_SCALE*y,0)));
-		if(z > q) {
-			if(z < WATER_HEIGHT) {
-				return vWater;
-			}
-			else {
-				return vAir;
-			}
-		}
-		else {
-			return vLand;
-		}
-	}
-
-	public VoxelDare.World CreateMiniMinecraft()
-	{
-		return Create(
-			new VoxelDare.Int3(0,0,0), new VoxelDare.Int3(16,16,8),
-			Vector3.one, FMiniMinecraft);
-	}
-
-	VoxelDare.Voxel FDiscworld(int x, int y, int z, int radius)
-	{
-		float r = Mathf.Sqrt(x*x + y*y);
-		if(r > radius) {
-			return vAir;
-		}
-		return FMiniMinecraft(x,y,z);
-	}
-
-	public VoxelDare.World CreateDiscworld()
-	{
-		const int radius = 16;
-		const int height = 8;
-		Vector3 scale = Vector3.one;// new Vector3(4,4,4);
-		// pass 1: solid
-		VoxelDare.World vw = Create(
-			new VoxelDare.Int3(-radius,-radius,0), new VoxelDare.Int3(radius,radius,height),
-			scale,
-			(x,y,z) => FDiscworld(x,y,z,radius));
-		// pass 2: bottom is bedrock
-		foreach(VoxelDare.Int3 i in vw.GetBottomVoxels()) {
-			vw.Set(i, vBedRock);
-		}
-		return vw;
-	}
-
-	VoxelDare.Voxel FRollingHills(int x, int y, int z)
+	VoxelDare.Voxel FMinecraft(int x, int y, int z)
 	{
 		const float XY_SCALE = 1.3f;
 		float q = Mathf.Max(0, 5.0f*(1.0f+perlin.Compute(XY_SCALE*x,XY_SCALE*y,0)));
@@ -105,6 +40,19 @@ public class WorldGenerator : MonoBehaviour
 			return vLand;
 		}
 	}
+	
+	public VoxelDare.World Create(VoxelDare.Int3 min, VoxelDare.Int3 max, Vector3 scale, Func<int,int,int,VoxelDare.Voxel> f)
+	{
+		VoxelDare.World w = new VoxelDare.World(scale);
+		for(int z=min.z; z<max.z; z++) {
+			for(int y=min.y; y<max.y; y++) {
+				for(int x=min.x; x<max.x; x++) {
+					w.Set(new VoxelDare.Int3(x,y,z), f(x,y,z));
+				}
+			}
+		}
+		return w;
+	}
 
 	public VoxelDare.World CreateRollingHills(int size, int height)
 	{
@@ -112,17 +60,12 @@ public class WorldGenerator : MonoBehaviour
 		return Create(
 			new VoxelDare.Int3(-radius,-radius,0), new VoxelDare.Int3(radius,radius,height),
 			Vector3.one,
-			(x,y,z) => FRollingHills(x,y,z));
+			(x,y,z) => FMinecraft(x,y,z));
 	}
 
 	void Start()
 	{
-		VoxelDare.World voxels = null;
-		switch(worldType) {
-			case WorldType.MINI_MINECRAFT: voxels = CreateMiniMinecraft(); break;
-			case WorldType.DISCWORLD: voxels = CreateDiscworld(); break;
-			case WorldType.ROLLINGHILLS: voxels = CreateRollingHills(worldSize, worldHeight); break;
-		};
+		VoxelDare.World voxels = CreateRollingHills(worldSize, worldHeight);
 		GetComponent<VoxelDare.VoxelRenderer>().Voxels = voxels;
 	}
 
